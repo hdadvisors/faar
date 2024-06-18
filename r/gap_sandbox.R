4f7aaaff118f814fec36edb9bd62d3f3e611ad2c
ericvmai <ericvmai@gmail.com>
2024-06-18 19:16:14 -0400

    Update gap_sandbox.R

--- r/gap_sandbox.R
+++ r/gap_sandbox.R
@@ -48,7 +48,6 @@ b25094 <- b25094_pull |>
     TRUE ~ "More than $2,000"
   ))
 
-
 b25094_region <- b25094 |> 
   group_by(monthly) |> 
   summarise(estimate = sum(estimate)) |> 
@@ -57,6 +56,9 @@ b25094_region <- b25094 |>
   mutate(pct = estimate/total) |> 
   mutate(vacant = pct*1049) |> 
   mutate(adj_estmate = estimate + vacant)
+         
+# The data visualization below shows the distribution of owner-occupied housing
+# based on monthly housing costs. This viz is focused on the entire region.
 
 custom_order <- factor(b25094_region$monthly, levels = c("Less than $599", "$600 to $999", "$1,000 to $1,499",
                                                          "$1,500 to $1,999", "More than $2,000"))
@@ -123,6 +125,9 @@ b25063 <- b25063_pull |>
 b25063_region <- b25063 |> 
   group_by(rent) |> 
   summarise(estimate = sum(estimate))
+
+# The data visualization below shows the distribution of renter-occupied housing
+# based on monthly housing costs. This viz is focused on the entire region.
 
 custom_order <- factor(b25063_region$rent, levels = c("Less than $599", "$600 to $999", "$1,000 to $1,499",
                                                          "$1,500 to $1,999", "More than $2,000"))
@@ -328,7 +333,8 @@ ggplot(b25127_region,
            fill = structure)) +
   geom_col() +
   theme_hda(base_size = 10) +
-  scale_fill_hda(direction = -1)
+  scale_fill_hda(direction = -1) +
+  facet_wrap(~tenure)
 
 
 # Estimating housing demand
@@ -356,9 +362,91 @@ pums_data <- get_pums(
 pums_fxburg <- pums_data |> 
   filter(PUMA20 %in% c("17700", "17900")) |> 
   filter(TEN_label != "N/A (GQ/vacant)") |> 
-  mutate(TEN_label = case_when(
-    TEN_label == "Rented" ~ "Renter"
+  mutate(tenure = case_when(
+    TEN_label == "Rented" ~ "Renter",
+    TEN_label == "Owned with mortgage or loan (include home equity loans)" ~ "Homeowner",
+    TEN_label == "Owned free and clear" ~ "Homeowner",
+    TEN_label == "Occupied without payment of rent" ~ "Renter"
+  )) |> 
+  mutate(pct_ami = HINCP/142300) |>
+  mutate(ami = case_when(
+    pct_ami <= 0.3 ~ "30% AMI or below",
+    pct_ami > 0.3 & pct_ami <= 0.5 ~ "31-50% AMI",
+    pct_ami > 0.5 & pct_ami <= 0.8 ~ "51-80% AMI",
+    pct_ami > 0.8 & pct_ami <= 1 ~ "81-100% AMI",
+    pct_ami > 1 & pct_ami <= 1.20 ~ "101-120% AMI",
+    pct_ami > 1.20 ~ "121% AMI and greater")) 
+
+
+
+supply <- pums_fxburg |> 
+  select(SPORDER, tenure, ami, pct_ami, SMOCP, GRNTP, ADJHSG, ADJINC, WGTP) |> 
+  mutate(ADJINC = as.numeric(ADJINC)) |> 
+  filter(SPORDER ==1) |> 
+  mutate(cost = case_when(
+    tenure == "Renter" ~ GRNTP,
+    tenure == "Homeowner" ~ SMOCP
+  )) |> 
+  mutate(cost = ADJINC * cost,
+         annual_cost = 12*cost,
+         income = (annual_cost*10)/3) |> 
+  mutate(pct_ami_unit = income/142300) |>
+  mutate(ami_unit = case_when(
+    pct_ami_unit <= 0.3 ~ "30% AMI or below",
+    pct_ami_unit > 0.3 & pct_ami_unit  <= 0.5 ~ "31-50% AMI",
+    pct_ami_unit > 0.5 & pct_ami_unit  <= 0.8 ~ "51-80% AMI",
+    pct_ami_unit > 0.8 & pct_ami_unit  <= 1 ~ "81-100% AMI",
+    pct_ami_unit > 1 & pct_ami_unit  <= 1.20 ~ "101-120% AMI",
+    pct_ami_unit > 1.20 ~ "121% AMI and greater")) |> 
+  select(WGTP, tenure, ami, ami_unit)
+
+
+
+fxburg_units <- supply |> 
+  group_by(ami_unit, tenure) |> 
+  summarise(estimate = sum(WGTP)) |> 
+  select(ami = ami_unit, tenure, estimate)
+  
+fxburg_hh <- supply |> 
+  group_by(ami, tenure) |> 
+  summarise(estimate = sum(WGTP)) 
+
+fxburg_supply <- fxburg_units |> 
+  full_join(fxburg_hh, by = c("ami", "tenure")) |> 
+  select(ami, tenure, hh = estimate.y, units = estimate.x) |> 
+  pivot_longer(cols = 3:4,
+               names_to = "value",
+               values_to = "estimate") |> 
+  mutate(value = case_when(
+    value == "hh" ~ "Demand",
+    value == "units" ~ "Supply"
   ))
+
+
+ami_order <- factor(fxburg_supply$ami, levels = c("30% AMI or below", "31-50% AMI",
+                                           "51-80% AMI", "81-100% AMI", "101-120% AMI",
+                                           "121% AMI and greater"))
+
+library(ggtext)
+
+title_text <- "<span style = 'color:#8baeaa'><b>Supply</b></span> and <span style = 'color:#445ca9'><b>Demand</span>"
+
+
+
+
+ggplot(fxburg_supply,
+       aes(x = ami_order,
+           y = estimate,
+           fill = value)) +
+  geom_col(position = "dodge") +
+  facet_wrap(~tenure) +
+  theme_hda(base_size = 10) +
+  scale_fill_hda() +
+  theme(legend.position = "right")
+
+
+
+
 
 
 
