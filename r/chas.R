08159117381c4188613be163052e02d56b621954
ericvmai <ericvmai@gmail.com>
2024-06-25 13:09:04 -0400

    Add CHAS data

--- r/chas.R
+++ r/chas.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(glue)
 library(httr)
 library(janitor)
+library(readxl)
 
 faar <- c("51630", "51033", "51099", "51177", "51179", "51137") # FAAR FIPS codes
 
@@ -74,42 +75,13 @@ walk(tables, function(table) {
       left_join(dict, by = c("Code" = "Column Name"))
     cleaned_with_dict
   })
-  file_name <- glue("Table{table}_2012to2019.csv")
+  file_name <- glue("Table{table}_2019to2020.csv")
   message(glue("Writing file {file_name}..."))
   write_csv(mytable, glue("data/{file_name}"))
 })
 
-t7_2020 <- read_csv("data/050/2020/2016thru2020-050-csv/Table7.csv") |>
-  clean_names() |>
-  mutate(fips = paste0(st, cnty)) |>
-  separate(name, into = c("county", "state"), sep = ",") |>
-  filter(st == "51") |>
-  pivot_longer(starts_with("T"),
-               names_to = "code",
-               values_to = "value") |>
-  mutate(id = str_extract(code, "\\d+$"),
-         type = str_extract(code, "est|moe"))|>
-  select(-code) |>
-  pivot_wider(names_from = type, values_from = value) |>
-  rename(Estimate = est, MOE = moe) |>
-  mutate(Code := paste0("T7_est",id)) |>
-  select(Code, Estimate, MOE, everything(), -id) |>
-  mutate(fips = case_when(
-    fips == "51515" ~ "51019",
-    TRUE ~ fips
-  )) |>
-  mutate(county = case_when(
-    county == "Bedford city" ~ "Bedford County",
-    TRUE ~ county
-  )) |>
-  subset(fips %in% faar) |> 
-  mutate(Year = 2020) 
 
-dict_2020 <- read_excel("data/050/2020/CHAS-data-dictionary-16-20.xlsx",
-                        sheet = "Table 7") 
-
-t7_clean <- t7_2020 |> 
-  left_join(dict_2020, by = c("Code" = "Column Name")) |> 
+cb_7 <- read_csv("data/Table7_2019to2020.csv")|> 
   clean_names() |> 
   filter(line_type == "Detail") |> 
   select(year, estimate, moe, county, fips, tenure, household_income, household_type, cost_burden) |> 
@@ -143,76 +115,11 @@ t7_clean <- t7_2020 |>
     TRUE ~ cost_burden
   )) 
 
-
-cb_7 <- read_csv("data/Table7_2012to2019.csv")|> 
-  clean_names() |> 
-  filter(line_type == "Detail") |> 
-  select(year, estimate, moe, county, fips, tenure, household_income, household_type, cost_burden) |> 
-  mutate(tenure = case_when(
-    tenure == "Owner occupied" ~ "Homeowner",
-    tenure == "Renter occupied" ~ "Renter"
-  )) |> 
-  mutate(household_income = case_when(
-    household_income == "household income is less than or equal to 30% of HAMFI" ~ "30% AMI or less",
-    household_income == "household income is greater than 30% but less than or equal to 50% of HAMFI" ~ "31 to 50% AMI",
-    household_income == "household income is greater than 50% but less than or equal to 80% of HAMFI" ~ "51 to 80% AMI",
-    household_income == "household income is greater than 80% but less than or equal to 100% of HAMFI" ~ "81 to 100% AMI",
-    household_income == "household income is greater than 100% of HAMFI" ~ "101% AMI or greater"
-  )) |> 
-  mutate(household_type = case_when(
-    household_type == "household type is elderly family (2 persons, with either or both age 62 or over)" ~ "Elderly family",
-    household_type == "household type is small family (2 persons, neither person 62 years or over, or 3 or 4 persons)" ~ "Small family",
-    household_type == "household type is large family (5 or more persons)" ~ "Large family",
-    household_type == "household type is elderly non-family" ~ "Elderly non-family",
-    household_type == "other household type (non-elderly non-family)" ~ "Non-elderly non-family"
-  ))  |> 
-  mutate(cost_burden = case_when(
-    cost_burden == "housing cost burden is less than or equal to 30%" ~ "Not cost-burdened",
-    cost_burden == "housing cost burden is greater than 30% but less than or equal to 50%" ~ "Cost-burdened",
-    cost_burden == "housing cost burden is greater than 50%" ~ "Severely cost-burdened",
-    cost_burden == "housing cost burden not computed (household has no/negative income)" ~ "No or negative income"
-  )) |> 
-  mutate(cb_group = case_when(
-    cost_burden == "Cost-burdened" ~ "Cost-burdened",
-    cost_burden == "Severely cost-burdened" ~ "Cost-burdened",
-    TRUE ~ cost_burden
-  )) 
-
-cb_7 <- rbind(cb_7, t7_clean)
 
 write_rds(cb_7, "data/cb_7.rds")
 
-t9_2020 <- read_csv("data/050/2020/2016thru2020-050-csv/Table9.csv") |>
-  clean_names() |>
-  mutate(fips = paste0(st, cnty)) |>
-  separate(name, into = c("county", "state"), sep = ",") |>
-  filter(st == "51") |>
-  pivot_longer(starts_with("T"),
-               names_to = "code",
-               values_to = "value") |>
-  mutate(id = str_extract(code, "\\d+$"),
-         type = str_extract(code, "est|moe"))|>
-  select(-code) |>
-  pivot_wider(names_from = type, values_from = value) |>
-  rename(Estimate = est, MOE = moe) |>
-  mutate(Code := paste0("T9_est",id)) |>
-  select(Code, Estimate, MOE, everything(), -id) |>
-  mutate(fips = case_when(
-    fips == "51515" ~ "51019",
-    TRUE ~ fips
-  )) |>
-  mutate(county = case_when(
-    county == "Bedford city" ~ "Bedford County",
-    TRUE ~ county
-  )) |>
-  subset(fips %in% faar) |> 
-  mutate(Year = 2020) 
 
-dict_2020 <- read_excel("data/050/2020/CHAS-data-dictionary-16-20.xlsx",
-                        sheet = "Table 9") 
-
-t9_clean <- t9_2020 |> 
-  left_join(dict_2020, by = c("Code" = "Column Name")) |> 
+cb_9 <- read_csv("data/Table9_2019to2020.csv") |> 
   clean_names() |> 
   filter(line_type == "Detail")  |> 
   mutate(tenure = case_when(
@@ -238,39 +145,17 @@ t9_clean <- t9_2020 |>
     TRUE ~ cost_burden
   )) |> 
   select(county, fips, year, race, cost_burden, cb_group, estimate, moe)
-
-
-
-cb_9 <- read_csv("data/Table9_2012to2019.csv") |> 
-  clean_names() |> 
-  filter(line_type == "Detail")  |> 
-  mutate(tenure = case_when(
-    tenure == "Owner occupied" ~ "Homeowner",
-    tenure == "Renter occupied" ~ "Renter"
-  )) |> 
-  mutate(race = case_when(
-    race_ethnicity == "White alone, non-Hispanic" ~ "White, non-Hispanic",
-    race_ethnicity == "Black or African-American alone, non-Hispanic" ~ "Black",
-    race_ethnicity == "Asian alone, non-Hispanic" ~ "Asian",
-    race_ethnicity == "Hispanic, any race" ~ "Hispanic or Latino",
-    TRUE ~ "Another race, including multiracial"
-  ))   |> 
-  mutate(cost_burden = case_when(
-    cost_burden == "less than or equal to 30%" ~ "Not cost-burdened",
-    cost_burden == "greater than 30% but less than or equal to 50%" ~ "Cost-burdened",
-    cost_burden == "greater than 50%" ~ "Severely cost-burdened",
-    cost_burden == "not computed (no/negative income)" ~ "No or negative income"
-  )) |> 
-  mutate(cb_group = case_when(
-    cost_burden == "Cost-burdened" ~ "Cost-burdened",
-    cost_burden == "Severely cost-burdened" ~ "Cost-burdened",
-    TRUE ~ cost_burden
-  )) |> 
-  select(county, fips, year, race, cost_burden, cb_group, estimate, moe)
-
-cb_9 <- rbind(cb_9, t9_clean)
 
 write_rds(cb_9, "data/cb_9.rds")
+
+tb_18a <- read_csv("data/Table18A_2019to2020.csv") |> 
+  clean_names() |> 
+  filter(line_type == "Detail")
+
+tb_18b <- read_csv("data/Table18B_2019to2020.csv")
+tb_18c <- read_csv("data/Table18C_2019to2020.csv")
+
+
 
 
 t18c_2020 <- read_csv("data/050/2020/2016thru2020-050-csv/Table18C.csv") |>
@@ -353,7 +238,8 @@ t18c_2020_clean <-  t18c_clean |>
   ))
 
 
-
+tb_18a <- read_csv("data/Table18C_2012to2019.csv")
+tb_18b <- read_csv("data/Table18C_2012to2019.csv")
 tb_18c <- read_csv("data/Table18C_2012to2019.csv")
 
 colnames(tb_18c)[16] <- "Cost"
