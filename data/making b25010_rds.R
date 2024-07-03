82b415498a689a7cb7ee299b04b4e8a10020b4c4
Maria Dougherty <125600190+mtedougherty@users.noreply.github.com>
2024-07-03 13:22:14 -0400

    renaming files, updating code to pull for 2010-2022, re-publishing rds file with added year data
    renaming files, updating code to pull for 2010-2022, re-publishing rds file with added year data

--- data/making b25010_rds.R
+++ data/making b25010_rds.R
@@ -56,3 +56,65 @@ print(current_dir)
 
 # Save the cleaned data as an RDS file
 write_rds(b25009_clean, "path/to/save/b25009_clean.rds")
+
+
+#issues with getting this data set where I want it, 
+
+## Going back to using B25010 instead of B25009 
+#Pulling from 2010-2022 
+
+cv <- c("51630", "51033", "51099", "51177", "51179", "51137") # FAAR FIPS codes
+
+years <- 2010:2022
+
+b25010_vars <- load_variables(2022, "acs5") |> 
+  filter(str_sub(name, end = 6) %in% "B25009")
+
+b25010_raw <- map_dfr(years, function(yr){
+  b25010_pull <- get_acs(
+    geography = "county",
+    state = "VA",
+    table = "B25009",
+    year = yr,
+    survey = "acs5",
+    cache_table = TRUE
+  ) |>
+    mutate(year = yr)
+})
+
+b25010_raw <- b25010_raw |> 
+  subset(GEOID %in% cv)
+
+b25010_vars_cleaned <- b25010_vars |> 
+  separate(label, into = c("est", "total", "tenure", "size"), sep = "!!") |> 
+  select(variable = name, tenure, size)  |> 
+  drop_na() |>
+  mutate(tenure = case_when(
+    tenure == "Owner occupied:" ~ "Homeowner",
+    tenure == "Renter occupied:" ~ "Renter"
+  ))
+
+b25010_data <- b25010_raw |> 
+  right_join(b25010_vars_cleaned, by = "variable") |> 
+  select(NAME, GEOID, year, tenure, size, estimate) |> 
+  mutate(NAME = str_remove_all(NAME, ", Virginia")) |> 
+  mutate(size = case_when(
+    size == "1-person household" ~ "1-person",
+    size == "2-person household" ~ "2-person",
+    size == "3-person household" ~ "3-person",
+    TRUE ~ "4 or more persons"
+  )) 
+
+# Preview the cleaned data
+head(b25010_data)
+
+
+# Print the current working directory
+current_dir <- getwd()
+print(current_dir)
+
+# Optionally set a new working directory
+# setwd("path/to/your/directory")
+
+# Save the cleaned data as an RDS file
+write_rds(b25010_data, "/Users/mted/Desktop/HDA/FAAR/faar/data/b25010_decade_region.rds")
